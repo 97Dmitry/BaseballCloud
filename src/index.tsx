a5f7ff1bd58c6352435177eeee1b4308591c194a
97Dmitry <dart_veyder_1997@mail.ru>
2021-07-10 16:45:10 +0600

    Added Apollo

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,27 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 import store, { persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 
 import Global from "./styles/globalStyle";
 
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io/",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Global />
-        <App />
+        <ApolloProvider client={client}>
+          <Global />
+          <App />
+        </ApolloProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
